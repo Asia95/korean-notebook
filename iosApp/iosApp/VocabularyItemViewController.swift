e8ae9cdb63d66ba89edcbec44ec3278aa94aaa5c
qaadmin <qaadmin@qaadmins-MacBook-Pro.local>
2019-06-23 10:36:15 +0200

    add flashcard button ios

--- iosApp/iosApp/VocabularyItemViewController.swift
+++ iosApp/iosApp/VocabularyItemViewController.swift
@@ -12,17 +12,37 @@ import koreanNotebook
 class VocabularyItemViewController: UIViewController {
 
     @IBOutlet weak var vocabularyTitle: UILabel!
+    var flashcardsBtn: UIBarButtonItem!
     @IBOutlet weak var vocabularyDescription: UILabel!
     var vocabularyItem: VocabularyEntry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        flashcardsBtn = UIBarButtonItem(image: UIImage(named: "card_icon.png"), style: .plain, target: self, action: #selector(onFlashcardAdd))
+        navigationItem.rightBarButtonItem = flashcardsBtn
+        
         if let vocabulary = vocabularyItem {
             vocabularyTitle.text = vocabulary.title
             vocabularyDescription.text = vocabulary.explanation
         }
     }
     
+    @objc func onFlashcardAdd(sender: UIBarButtonItem) {
+        showToast(controller: self, message : "Added to flashcards", seconds: 2.0)
+    }
+    
+    func showToast(controller: UIViewController, message : String, seconds: Double) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = UIColor.black
+        alert.view.alpha = 0.6
+        alert.view.layer.cornerRadius = 15
+        
+        controller.present(alert, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
 
     /*
     // MARK: - Navigation
